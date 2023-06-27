174d8ded295b1fd145971f8e31ec4f043ddd8ce6
James Brock <jamesbrock@gmail.com>
2023-06-27 12:33:22 +0900

    Cleanup

--- nix/pyedifice/default.nix
+++ nix/pyedifice/default.nix
@@ -1,34 +1,61 @@
 { lib
 , buildPythonPackage
-, fetchPypi
 , pytest
 , qasync
 , pyside6
+, pyqt6
 , watchdog
 , numpy
-, pyqt6
 }:
 
 buildPythonPackage rec {
-    pname = "pyedifice";
-    version = "0.0.10";
+  pname = "pyedifice";
+  version = "0.0.10";
 
-    # src = pkgs.fetchPypi {
-    #   inherit pname version;
-    #   sha256 = "sha256-u46Ca/UgEuoAQ9HHQZmYgX/htUEbxp3XaVEXZ3iMZdI=";
-    # };
+  # src = pkgs.fetchPypi {
+  #   inherit pname version;
+  #   sha256 = "sha256-u46Ca/UgEuoAQ9HHQZmYgX/htUEbxp3XaVEXZ3iMZdI=";
+  # };
 
-    src = ../..;
+  # src = ../..;
 
-    propagatedBuildInputs = [
-      qasync
-      pyside6
-      pyqt6
-      watchdog
-      numpy
-    ];
+  # src = lib.cleanSourceWith {
+  #   src = ../..;
+  #   filter = path: _: builtins.elem path [
+  #     "edifice"
+  #     "LICENSE"
+  #     "README.md"
+  #     "requirements.txt"
+  #     "setup.py"
+  #   ];
+  # };
 
-    doCheck = false;
+  # src = lib.sources.sourceByRegex ../.. [
+  #   "edifice/.*\\.py"
+  #   "LICENSE"
+  #   "README\\.md"
+  #   "requirements\\.txt"
+  #   "setup\\.py"
+  # ];
+
+  src = lib.cleanSourceWith {
+    src = lib.cleanSource ../..;
+    filter = path: _type: ! (builtins.elem (builtins.baseNameOf path) [
+      "__pycache__"
+      "flake.nix"
+      "flake.lock"
+    ]);
+  };
+
+  propagatedBuildInputs = [
+    qasync
+    pyside6
+    pyqt6
+    watchdog
+    numpy
+  ];
+
+  doCheck = false;
 
   meta = with lib; {
     description = "declarative GUI library for Python";
