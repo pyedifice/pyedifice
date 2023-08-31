46d3f651e301394c655ddaafbb2acefd916cc80d
James Brock <jamesbrock@gmail.com>
2023-08-31 14:45:43 +0900

    Github Actions Poetry CI
    nix run .#run_tests, nix run .#run_tests-virtualX

--- flake.nix
+++ flake.nix
@@ -1,5 +1,7 @@
 {
   inputs = {
+    # Before we try to upgrade nixpkgs we better make sure this is fixed:
+    # https://github.com/nix-community/poetry2nix/issues/1291#issuecomment-1702272801
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
     flake-utils.url = "github:numtide/flake-utils";
     poetry2nix = {
@@ -28,7 +30,6 @@
           };
         in pkgs.python310.override { inherit packageOverrides; self = pythonOverride; };
 
-
       pythonWithPackages = pythonOverride.withPackages (p: with p; [
         pip # for reading dependency information with pip list
         pytest
@@ -39,42 +40,31 @@
         watchdog
       ]);
 
-      # qtOverride = attrs:
-      #   let
-      #     libraries = [
-      #       pkgs.libGL
-      #       pkgs.stdenv.cc.cc.lib
-      #       pkgs.glib
-      #       pkgs.zlib
-      #       "/run/opgengl-driver"
-      #       pkgs.libxkbcommon
-      #       pkgs.fontconfig
-      #       pkgs.xorg.libX11
-      #       pkgs.freetype
-      #       pkgs.dbus
-      #     ];
-      #   in
-      #   attrs // {
-      #     # https://github.com/NixOS/nixpkgs/issues/80147#issuecomment-784857897
-      #     QT_PLUGIN_PATH = with pkgs.qt6; "${qtbase}/${qtbase.qtPluginPrefix}";
-      #     # fixes libstdc++ issues and libgl.so issues
-      #     LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath libraries}";
-      #   };
-
       qtOverride = attrs: attrs // {
           # https://github.com/NixOS/nixpkgs/issues/80147#issuecomment-784857897
           QT_PLUGIN_PATH = with pkgs.qt6; "${qtbase}/${qtbase.qtPluginPrefix}";
         };
 
-
       pythonEnv = qtOverride pythonWithPackages.env;
 
-      # pythonEnv = pythonWithPackages.env.overrideAttrs(finalAttrs: prevAttrs: {
-      #   # https://github.com/NixOS/nixpkgs/issues/80147#issuecomment-784857897
-      #   QT_PLUGIN_PATH = with pkgs.qt6; "${qtbase}/${qtbase.qtPluginPrefix}";
-      # });
+      poetryEnvAttrs = {
+        # We cannot currently specify the python version because of
+        # https://github.com/nix-community/poetry2nix/issues/1076
+        projectDir = ./.;
+        preferWheels = true;
+        overrides = pkgs.poetry2nix.overrides.withDefaults
+          (pyfinal: pyprev: {
+            # When we're building with Nix, use the pyside6 from nixpkgs,
+            # not the one from PyPI, because I can't figure out how to
+            # link it with Qt.
+            pyside6 = pyfinal.pkgs.python3.pkgs.pyside6;
+            shiboken6 = pyfinal.pkgs.python3.pkgs.shiboken6;
+            pyqt6 = pyfinal.pkgs.python3.pkgs.pyqt6;
+            pyqt6-sip = pyfinal.pkgs.python3.pkgs.pyqt6-sip;
+          });
+      };
     in
-    rec {
+    {
       # There are 3 devShell flavors here.
       #
       # 1. nix develop .#default
@@ -95,7 +85,11 @@
       #
       # 3. nix develop .#poetry2nix
       #
-      #    Not working.
+      #    https://github.com/nix-community/poetry2nix#mkpoetryenv
+      #    environment.
+      #    In this environment the tests should pass.
+      #
+      #        ./run_tests.sh
       #
       devShells = {
 
@@ -130,34 +124,52 @@
             '';
         };
 
-        poetry2nix = (pkgs.poetry2nix.mkPoetryEnv {
-          projectDir = ./.;
+        poetry2nix = (pkgs.poetry2nix.mkPoetryEnv (poetryEnvAttrs // {
           editablePackageSources = {
             edifice = ./edifice;
           };
-          preferWheels = true;
           extras = [ "*" ];
-        }).env.overrideAttrs(oldAttrs: {
-          propagatedBuildInputs = [
-            pkgs.libxkbcommon
-          ];
-        });
+          extraPackages = ps: [ ps.pip ];
+        })).env;
+
       };
 
       lib = {
         qasync = qasync_;
         pyedifice = pyedifice_;
       };
-      # apps = {
-      #   test = {
-      #     type = "app";
-      #     program = (pythonOverride.withPackages (p: [p.pyedifice]).env) // {
-      #       shellHook = ''
-      #         ./run_tests.sh
-      #         '';
-      #       };
-      #   };
-      # };
+
+      apps =
+      let
+        run_tests_sh = pkgs.writeScript "run_tests_sh" (builtins.readFile ./run_tests.sh);
+        script = pkgs.writeShellApplication {
+          name = "edifice-run-tests";
+          runtimeInputs = [
+            (pkgs.poetry2nix.mkPoetryEnv poetryEnvAttrs)
+          ];
+          text = "${run_tests_sh}";
+        };
+        script-virtualX = pkgs.writeShellApplication {
+          name = "edifice-run-tests";
+          runtimeInputs = [
+            (pkgs.poetry2nix.mkPoetryEnv poetryEnvAttrs)
+            pkgs.xvfb-run
+          ];
+          text = "xvfb-run ${run_tests_sh}";
+        };
+      in
+      {
+        run_tests =
+        {
+          type = "app";
+          program = "${script}/bin/edifice-run-tests";
+        };
+        run_tests-virtualX =
+        {
+          type = "app";
+          program = "${script-virtualX}/bin/edifice-run-tests";
+        };
+      };
     });
 }
 
