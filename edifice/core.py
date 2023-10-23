0e804477d29ad19eb51ceea984e4b06ee6231173
Viktor Kronvall <viktor.kronvall@gmail.com>
2023-10-23 18:51:07 +0900

    WIP: QtWidgets

--- edifice/core.py
+++ edifice/core.py
@@ -1,7 +1,14 @@
+import asyncio
 from collections.abc import Callable
+import signal
 import sys
 import threading
 from typing import Generic, ParamSpec, TypeVar, cast, overload, Literal, Optional
+import functools
+from PySide6.QtWidgets import QApplication, QHBoxLayout, QLabel, QMainWindow, QVBoxLayout, QWidget
+from PySide6.QtAsyncio import QAsyncioEventLoopPolicy
+
+from PySide6.QtCore import QObject, Signal, Slot
 
 local_state = threading.local()
 
@@ -37,22 +44,27 @@ class Tracker:
 W = TypeVar("W")
 P = ParamSpec("P")
 
+CHILDREN: str = "children"
+
 class Element(Generic[W]):
     def __init__(
-        self, component: "Component",
+        self,
+        component: "Component",
         name: str,
         args=None,
         kwargs=None,
     ) -> None:
         self.component = component
         self._current_context = None
-        self.children = []
         self.args = args or []
         self.kwargs = kwargs or {}
         self.name = name
         ctx = get_render_context(required=False)
         if ctx is not None and len(ctx.trackers) > 0:
             ctx.trackers[-1].add(self)
+        else:
+            if ctx is not None:
+                print(ctx.trackers)
         print("init", self.name)
 
     def __enter__(self):
@@ -67,24 +79,46 @@ class Element(Generic[W]):
         ctx = get_render_context()
         tracker = ctx.trackers.pop()
         children = tracker.collect()
+        # Mutate kwargs to include children
+        print(self.name, children)
+        if CHILDREN not in self.kwargs:
+            self.kwargs[CHILDREN] = []
         for child in children:
-            self.children.append(child)
+            self.kwargs[CHILDREN].append(child)
         ctx.element = self
 
     def __repr__(self) -> str:
         name = self.name
-        kids = [c.__repr__() for c in self.children]
+        kids = [c.__repr__() for c in self.kwargs.get(CHILDREN, [])]
         children = ", ".join(kids)
         return f"{name}({children})"
 
-class Component:
+class FunctionComponent:
     def __init__(self, name: str, render: Callable[..., Element | None]) -> None:
         self.render = render
         self.name = name
+        functools.update_wrapper(self, render)
 
     def __call__(self, *args, **kwargs):
         element: Element = Element(self, name=self.name, args=args, kwargs=kwargs)
         return element
+
+class QtComponent:
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    def __call__(self, *args, **kwargs) -> Element:
+        el: Element = Element(self, name=self.name, args=args, kwargs=kwargs)
+        return el
+
+class RowComponent(QtComponent):
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.widget = QWidget()
+        self.layout = QHBoxLayout(self.widget)
+
+Component = FunctionComponent | QtComponent
+
 
 F = TypeVar("F", bound=Callable[..., Element])
 
@@ -111,7 +145,7 @@ def component(
 ):
     def wrapper(fn: Callable[P, Element | None] | F) -> F:
         name = fn.__name__
-        component = Component(name, render=fn)
+        component = FunctionComponent(name, render=fn)
         # A component is a Callable[..., Element]
         return cast(F, component)
     if fn is not None:
@@ -133,7 +167,11 @@ class _RenderContext:
     def _render(self, element: Element):
         self.trackers = []
         with container() as main:
-            root = element.component.render(*element.args, **element.kwargs)
+            match element.component:
+                case FunctionComponent():
+                    root = element.component.render(*element.args, **element.kwargs)
+                case QtComponent():
+                    raise NotImplementedError
         if root is None:
             root = main
 
@@ -153,9 +191,12 @@ def container():
 
 # Example components
 
-@component
-def Row():
-    pass
+def Row(children: tuple[Element, ...] = ()):
+    name = "Row"
+    print(children)
+    component = RowComponent(name)
+    return Element(component, name)
+
 
 @component
 def Label(text: str):
@@ -189,11 +230,31 @@ def example():
                 print(f"inner={inner}")
 
 def main():
+    QApplication(sys.argv)
+    main_window = QMainWindow()
     el = View()
-    print("CTX")
     ctx = _RenderContext(el)
     ctx.render(el)
-    print(ctx.element)
+    main_window.show()
+
+    # row = QWidget()
+    # main_window.setCentralWidget(row)
+    # box = QVBoxLayout(row)
+    # box.addWidget(QLabel(text="asd"))
+    # row2 = QWidget()
+    # box2 = QHBoxLayout(row2)
+    # box.addWidget(row2)
+    # col = QWidget()
+    # box2.addWidget(col)
+    # col2 = QWidget()
+    # box2.addWidget(col2)
+    # box3 = QVBoxLayout(col)
+    # box3.addWidget(QLabel(text="fgh"))
+    # box4 = QVBoxLayout(col2)
+    # box4.addWidget(QLabel(text="jkl"))
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+    asyncio.set_event_loop_policy(QAsyncioEventLoopPolicy())
+    asyncio.get_event_loop().run_forever()
 
 if __name__ == "__main__":
     main()
