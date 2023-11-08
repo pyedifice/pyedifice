12069d6e7869008a7f01cf437b8489af3fbdb3a1
James Brock <jamesbrock@gmail.com>
2023-11-08 12:57:12 +0900

    mv components to base_components

--- edifice/base_components/__init__.py
+++ edifice/base_components/__init__.py
@@ -36,3 +36,7 @@ from .base_components import (QtWidgetElement, Window, View, ScrollView, TabView
 															GridView, Label, Image, ImageSvg, Icon, IconButton, Button,
 															TextInput, CheckBox, RadioButton, Slider, Dropdown,
 															CustomWidget)
+from .button_view import ButtonView
+from .flow_view import FlowView
+from .image_aspect import ImageAspect
+from .table_grid_view import TableGridView
