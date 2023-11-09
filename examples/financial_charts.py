a11ae421a18e4f3567cfd11648a2a7b69004fc3e
James Brock <jamesbrock@gmail.com>
2023-11-10 02:35:44 +0900

    with-hooks style: financial_charts, harmonic_oscillator

--- examples/financial_charts.py
+++ examples/financial_charts.py
@@ -5,142 +5,164 @@ For each plot, the x and y axis could represent any field of any stock.
 Everything is reactive -- changes will automatically be reflected in the entire application.
 """
 
+#
+# pip install pandas yfinance matplotlib
+#
+# python examples/harmonic_oscillator.py
+#
+
+import sys, os
+# We need this sys.path line for running this example, especially in VSCode debugger.
+sys.path.insert(0, os.path.join(sys.path[0], '..'))
+import asyncio
+from collections import OrderedDict
+import typing as tp
 import edifice as ed
 from edifice import Dropdown, IconButton, Label, ScrollView, Slider, TextInput, View
 from edifice.components import plotting
 
-import matplotlib.colors as mcolors
+import matplotlib.colors
 import pandas as pd
 import yfinance as yf
 
 
-def _create_state_for_plot(plotname):
-    """Creates the state associated with a particular plot."""
+def _create_state_for_plot() -> dict[str, tp.Any]:
+    """Creates the default params associated with a plot."""
     return {
         # Data to show on x-axis (data type, data source)
-        f"{plotname}.xaxis.data": ("Date", "AAPL"),
+        "xaxis.data": ("Date", "AAPL"),
         # Transformation applied to x-axis (transform type, transform param)
-        f"{plotname}.xaxis.transform": ("None", 30),
-        f"{plotname}.yaxis.data": ("Close", "AAPL"),
-        f"{plotname}.yaxis.transform": ("None", 30),
+        "xaxis.transform": ("None", 30),
+        "yaxis.data": ("Close", "AAPL"),
+        "yaxis.transform": ("None", 30),
         # Plot type (line or scatter) and plot color
-        f"{plotname}.type": "line",
-        f"{plotname}.color": "peru",
+        "type": "line",
+        "color": "peru",
     }
-
-def merge(d1, d2):
-    """Helper function to merge two dictionaries."""
-    d = d1.copy()
-    d.update(d2)
-    return d
-
-
-# We store application state centrally. Each component can query this central store and
-# set state, and all components that use that state will automatically update.
-# See https://pyedifice.github.io/state.html
-app_state = ed.StateManager(merge(_create_state_for_plot("plot0"), {
-    "all_plots": ["plot0"],
-    "next_i": 1,
-}))
-
 
 # We create a component which describes the options for each axis (data source, transform).
 # Since this component owns no state, we can simply write a render function and use the
 # component decorator.
 @ed.component
-def AxisDescriptor(self, name, key, children):
+def AxisDescriptor(self, name, key, plot, plots_set): #TODO does plots_set change on every render?
     # We subscribe to app_state, so that state changes would trigger a re-render
     # Subscribe returns the data stored in app_state
-    data = app_state.subscribe(self, f"{key}.data")
-    data_type, ticker = data.value
-    transform = app_state.subscribe(self, f"{key}.transform")
-    transform_type, param = transform.value
+    data = plot[f"{key}.data"]
+    data_type, ticker = data
+    transform = plot[f"{key}.transform"]
+    transform_type, param = transform
     # We can use CSS styling. See https://pyedifice.github.io/styling.html
     row_style = {"align": "left", "width": 350}
-    return View(layout="column")(
-        View(layout="row", style=row_style)(
-            Label(name, style={"width": 40}),
+
+    def handle_data_type(data_type_text):
+        plot[f"{key}.data"] = (data_type_text, ticker)
+        plots_set(lambda x: x)
+
+    def handle_ticker(ticker_text):
+        plot[f"{key}.data"] = (data_type, ticker_text)
+        plots_set(lambda x: x)
+
+    def handle_transform_type(transform_type_text):
+        plot[f"{key}.transform"] = (transform_type_text, param)
+        plots_set(lambda x: x)
+
+    def handle_param(param_val):
+        plot[f"{key}.transform"] = (transform_type, param_val)
+        plots_set(lambda x: x)
+
+
+    with View(layout="column"):
+        with View(layout="row", style=row_style):
+            Label(name, style={"width": 40})
             Dropdown(selection=data_type, options=["Date", "Close", "Volume"],
-                     on_select=lambda text: data.set((text, ticker))),
-            # if data_type != "Date", the following evaluates to False due to and short-circuiting.
-            # A False or None child is treated as an empty slot
-            data_type != "Date" and TextInput(
-                text=ticker, style={"padding": 2},
-                # The on_change callback is called whenever the change event fires,
-                # i.e. when the input box text changes.
-                on_change=lambda text: data.set((data_type, text))
+                     on_select=handle_data_type
             )
-        ),
-        View(layout="row", style=row_style)(
-            Label("Transform:", style={"width": 70}),
+            if data_type != "Date":
+                TextInput( text=ticker, style={"padding": 2},
+                    on_change=handle_ticker
+                )
+
+        with View(layout="row", style=row_style):
+            Label("Transform:", style={"width": 70})
             Dropdown(selection=transform_type, options=["None", "EMA"],
-                     on_select=lambda text: transform.set((text, param))),
-            transform == "EMA" and Label(f"Half Life ({param} days)", style={"width": 120}),
-            transform == "EMA" and Slider(
-                value=param, min_value=1, max_value=90, dtype=int,
-                on_change=lambda val: transform.set((transform_type, val))
-            )
-        )
-    )
+                     on_select=handle_transform_type)
+            if transform == "EMA":
+                Label(f"Half Life ({param} days)", style={"width": 120})
+            if transform == "EMA":
+                Slider(
+                    value=param, min_value=1, max_value=90, dtype=int,
+                    on_change=handle_param
+                )
 
 
 # We create a shorthand for creating a component with a label
 def labeled_elem(label, comp):
-    return View(layout="row", style={"align": "left"})(
-        Label(label, style={"width": 80}), comp,
-    )
+    with View(layout="row", style={"align": "left"}):
+        Label(label, style={"width": 80})
+        comp()
 
 def add_divider(comp):
-    return View(layout="column")(
-        comp,
+    with View(layout="column"):
+        comp()
         View(style={"height": 0, "border": "1px solid gray"})
-    )
 
 
 # Now we make a component to describe the entire plot: the descriptions of both axis,
 # plot type, and color
 @ed.component
-def PlotDescriptor(self, name, children):
-    plot_type = app_state.subscribe(self, f"{name}.type")
-    color = app_state.subscribe(self, f"{name}.color")
-    return View(layout="row", style={"margin": 5})(
-        View(layout="column", style={"align": "top"})(
-            AxisDescriptor("x-axis", f"{name}.xaxis"),
-            AxisDescriptor("y-axis", f"{name}.yaxis"),
-        ),
-        View(layout="column", style={"align": "top", "margin-left": 10})(
+def PlotDescriptor(self, plot, plots_set):
+    plot_type = plot["type"]
+    color = plot["color"]
+
+    def handle_plot_type(plot_type_text):
+        plot["type"] = plot_type_text
+        plots_set(lambda x: x)
+
+    def handle_color(color_text):
+        plot["color"] = color_text
+        plots_set(lambda x: x)
+
+    with View(layout="row", style={"margin": 5}):
+        with View(layout="column", style={"align": "top"}):
+            AxisDescriptor("x-axis", "xaxis", plot, plots_set)
+            AxisDescriptor("y-axis", "yaxis", plot, plots_set)
+        with View(layout="column", style={"align": "top", "margin-left": 10}):
             labeled_elem(
                 "Chart type",
-                Dropdown(selection=plot_type.value, options=["scatter", "line"],
-                         on_select=lambda text: plot_type.set(text))
-            ),
+                lambda: Dropdown(selection=plot_type, options=["scatter", "line"],
+                         on_select=handle_plot_type)
+            )
             labeled_elem(
                 "Color",
-                Dropdown(selection=color.value, options=list(mcolors.CSS4_COLORS.keys()),
-                         on_select=lambda text: color.set(text))
+                lambda: Dropdown(
+                    selection=color,
+                    options=list(matplotlib.colors.CSS4_COLORS.keys()),
+                    on_select=handle_color
+                )
             )
-        ),
-    )
 
 
 # Finally, we create a component that contains the plot descriptions, a button to add a plot,
 # and the actual Matplotlib figure.
-# To better organize the code, we create a class so that we can put plotting logic in methods.
-class App(ed.Element):
+@ed.component
+def App(self):
+
+    next_i, next_i_set = ed.use_state(int(1))
+    # We're cheating a bit here with the plots state.
+    # plots is a mutable dictionary, so we mutate the dictionary
+    # and then call plots_set to notify Edifice that it changed.
+    plots, plots_set = ed.use_state(OrderedDict([(int(0), _create_state_for_plot())]))
 
     # Adding a plot is very simple conceptually (and in Edifice).
     # Just add new state for the new plot!
-    def add_plot(self, e):
-        next_key = "plot" + str(app_state["next_i"])
-        app_state.update(merge(_create_state_for_plot(next_key), {
-            "all_plots": app_state["all_plots"] + [next_key],
-            "next_i": app_state["next_i"] + 1,
-        }))
+    def add_plot(e):
+        plots.update([(next_i, _create_state_for_plot())])
+        next_i_set(lambda j: j+1)
+        plots_set(plots) # sets to the same mutable OrderedDict
 
     # The Plotting function called by the plotting.Figure component.
     # The plotting function is passed a Matplotlib axis object.
-    def plot(self, ax):
-        all_plots = app_state["all_plots"]
+    def plot_figure(ax):
 
         def get_data(df, label, transform, param):
             if label == "Date":
@@ -149,14 +171,15 @@ class App(ed.Element):
                 return df[label]
             return df[label].ewm(halflife=param).mean()
 
-        for plot in all_plots:
-            xdata, xticker = app_state.subscribe(self, f"{plot}.xaxis.data").value
-            xtransform, xparam = app_state.subscribe(self, f"{plot}.xaxis.transform").value
-            ydata, yticker = app_state.subscribe(self, f"{plot}.yaxis.data").value
-            ytransform, yparam = app_state.subscribe(self, f"{plot}.yaxis.transform").value
-            plot_type = app_state.subscribe(self, f"{plot}.type").value
-            color = app_state.subscribe(self, f"{plot}.color").value
+        for plot in plots.values():
+            xdata, xticker = plot["xaxis.data"]
+            xtransform, xparam = plot["xaxis.transform"]
+            ydata, yticker = plot["yaxis.data"]
+            ytransform, yparam = plot["yaxis.transform"]
+            plot_type = plot["type"]
+            color = plot["color"]
 
+            # TODO fetch data asynchronously with use_async
             xdf = yf.Ticker(xticker).history("1y")
             ydf = yf.Ticker(yticker).history("1y")
 
@@ -168,23 +191,21 @@ class App(ed.Element):
             elif plot_type == "scatter":
                 ax.scatter(df.xdata, df.ydata, color=color)
 
-    def render(self):
-        all_plots = app_state.subscribe(self, "all_plots").value
-        return ed.Window(title="Financial Charts")(
-            View(layout="column", style={"margin": 10})(
-                ScrollView(layout="column")(
-                    *[add_divider(PlotDescriptor(plotname)) for plotname in all_plots]
-                ),
-                # Edifice comes with Font-Awesome icons for your convenience
-                IconButton(name="plus", title="Add Plot", on_click=self.add_plot),
-                # We create a lambda fuction so that the method doesn't compare equal to itself.
-                # This forces re-renders everytime this entire component renders.
-                plotting.Figure(lambda ax: self.plot(ax)),
-            )
-        )
+    with ed.Window(title="Financial Charts"):
+        with View(layout="column", style={"margin": 10}):
+            with ScrollView(layout="column"):
+                for plot in plots.values():
+                    add_divider(lambda: PlotDescriptor(plot, plots_set))
+
+            # Edifice comes with Font-Awesome icons for your convenience
+            IconButton(name="plus", title="Add Plot", on_click=add_plot)
+            # We create a lambda fuction so that the method doesn't compare equal to itself.
+            # This forces re-renders everytime this entire component renders.
+            plotting.Figure(lambda ax: plot_figure(ax))
 
 
 # Finally to start the the app, we pass the Element to the edifice.App object
 # and call the start function to start the event loop.
 if __name__ == "__main__":
-    ed.App(App(), inspector=True).start()
+    # ed.App(App(), mount_into_window=False, inspector=True).start()
+    ed.App(App(), mount_into_window=False, inspector=False).start()
--- examples/harmonic_oscillator.py
+++ examples/harmonic_oscillator.py
@@ -1,10 +1,21 @@
+#
+# python examples/harmonic_oscillator.py
+#
+
+import sys, os
+# We need this sys.path line for running this example, especially in VSCode debugger.
+sys.path.insert(0, os.path.join(sys.path[0], '..'))
+import asyncio
 import edifice as ed
 
 from edifice.components import plotting
 import numpy as np
 
+# We create time range once so we don't have to recreate it each plot
+time_range = np.linspace(0, 10, num=120)
 
-class Oscillator(ed.Element):
+@ed.component
+def Oscillator(self):
     """
     The high level logic is: there are two sets of state, spring parameters, and simulation parameters.
     The spring parameters can be changed by a slider for each of the parameters.
@@ -16,70 +27,65 @@ class Oscillator(ed.Element):
     based on current simulation time.
     """
 
-    def __init__(self):
-        super().__init__()
-        # Spring parameters: frequency and damping factor. Damping must be negative
-        self.angular_frequency = 2
-        self.damping = -0.5
+    # Spring parameters: frequency and damping factor. Damping must be negative
+    angular_frequency, angular_frequency_set = ed.use_state(2)
+    damping, damping_set = ed.use_state(-0.5)
 
-        # We create time range once so we don't have to recreate it each plot
-        self.time_range = np.linspace(0, 10, num=120)
-        self.is_playing = False
-        self.simulation_time = 0
-        self.dt = 0.03
+    is_playing, is_playing_set = ed.use_state(False)
+    play_trigger, play_trigger_set = ed.use_state(False)
+    simulation_time, simulation_time_set = ed.use_state(0.0)
+    dt = 0.03
 
-        # Animation timer. It simply adds dt to self.simulation_time each tick
-        self.timer = ed.Timer(lambda: self.set_state(simulation_time=self.simulation_time + self.dt))
-
-    def plot(self, ax):
+    def plot(ax):
         # Plotting function, called by the Matplotlib component. This is standard matplotlib code.
-        ax.plot(self.time_range, self.calculate_harmonic_motion(self.time_range))
+        ax.plot(time_range, calculate_harmonic_motion(time_range))
 
-    def calculate_harmonic_motion(self, t):
+    def calculate_harmonic_motion(t):
         # Formula for harmonic motion. We use complex numbers because the formula is nicer this way
         # (no special cases)
-        sqrt_expr = np.ones([], dtype=complex) * self.damping ** 2 - 4 * self.angular_frequency ** 2
-        complex_phase = (self.damping - np.sqrt(sqrt_expr)) / 2
+        sqrt_expr = np.ones([], dtype=complex) * damping ** 2 - 4 * angular_frequency ** 2
+        complex_phase = (damping - np.sqrt(sqrt_expr)) / 2
         return np.real(np.exp(complex_phase * t))
 
-    def did_render(self):
-        # Based on the is_playing status, we check whether or not to start or stop the timer.
-        if self.is_playing:
-            self.timer.start(int(self.dt * 1000))
-        else:
-            self.timer.stop()
+    # # Animation timer. It simply adds dt to simulation_time each tick
+    async def play_tick():
+        if is_playing:
+            simulation_time_set(lambda t: t + dt)
+            await asyncio.sleep(dt)
+            play_trigger_set(lambda p: not p)
 
-    def will_dismount(self):
-        # Cleanup function
-        self.timer.stop()
+    ed.use_async(play_tick, (is_playing, play_trigger))
 
-    def render(self):
-        return ed.View(layout="row")(
-            ed.View(layout="column", style={"width": 720, "margin": 10}) (
-                ed.View(layout="row")(
-                    ed.IconButton("pause" if self.is_playing else "play",
-                                  on_click=lambda e: self.set_state(is_playing=not self.is_playing)),
-                    ed.Button("Reset", on_click=lambda e: self.set_state(simulation_time=0)),
-                ),
-                plotting.Figure(lambda figure: self.plot(figure)),
-                ed.View(layout="row", style={"margin": 10})(
-                    ed.Label("Angular Frequency"),
-                    ed.Slider(value=self.angular_frequency, min_value=1, max_value=10,
-                              on_change=lambda value: self.set_state(angular_frequency=value, simulation_time=0)),
-                    ed.Label("Damping Factor"),
-                    ed.Slider(value=self.damping, min_value=-3, max_value=0,
-                              on_change=lambda value: self.set_state(damping=value, simulation_time=0)),
-                ),
-                # We position the ball and the centroid using absolute positioning.
-                # The label and ball offsets are different since we have to take into-account the size of the ball
-                ed.View(layout="none", style={"width": 720, "height": 10, "margin-top": 40}) (
-                    ed.Icon("bowling-ball", size=20, color=(255, 0, 0, 255),
-                            style={"left": 350 +  200 * self.calculate_harmonic_motion(self.simulation_time)}),
-                    ed.Label("|", style={"left": 356, "font-size": 20, "color": "blue"}),
-                )
-            )
-        )
+    def freq_slider_change(value):
+        angular_frequency_set(value)
+        simulation_time_set(0)
 
+    def damp_slider_change(value):
+        damping_set(value)
+        simulation_time_set(0)
+
+    with ed.View(layout="row"):
+        with ed.View(layout="column", style={"width": 720, "margin": 10}):
+            with ed.View(layout="row"):
+                ed.IconButton("pause" if is_playing else "play",
+                                on_click=lambda e: is_playing_set(lambda p: not p))
+                ed.Button("Reset", on_click=lambda e: simulation_time_set(0))
+            plotting.Figure(lambda figure: plot(figure))
+            with ed.View(layout="row", style={"margin": 10}):
+                ed.Label("Angular Frequency")
+                ed.Slider(value=angular_frequency, min_value=1, max_value=10,
+                            on_change=freq_slider_change)
+                ed.Label("Damping Factor")
+                ed.Slider(value=damping, min_value=-3, max_value=0,
+                            on_change=damp_slider_change)
+
+            # We position the ball and the centroid using absolute positioning.
+            # The label and ball offsets are different since we have to take into
+            # account the size of the ball
+            with ed.View(layout="none", style={"width": 720, "height": 10, "margin-top": 40}):
+                ed.Icon("bowling-ball", size=20, color=(255, 0, 0, 255),
+                        style={"left": 350 +  200 * calculate_harmonic_motion(simulation_time)})
+                ed.Label("|", style={"left": 356, "font-size": 20, "color": "blue"})
 
 if __name__ == "__main__":
-    ed.App(Oscillator()).start()
+    ed.App(Oscillator(), inspector=True).start()
