02ea3687e06ecfa8eb46096c6ecabbc9d628623b
James Brock <jamesbrock@gmail.com>
2023-11-18 16:50:41 +0900

    Calculator example extra View for hotreload

--- examples/calculator_hotreload.py
+++ examples/calculator_hotreload.py
@@ -4,10 +4,11 @@
 import os, sys
 # We need this sys.path line for running this example, especially in VSCode debugger.
 sys.path.insert(0, os.path.join(sys.path[0], '..'))
-from edifice import Window, component
+from edifice import Window, component, View
 from calculator import Calculator
 
 @component
 def Main(self):
     with Window(title="Calculator"):
-        Calculator()
+        with View():
+            Calculator()
