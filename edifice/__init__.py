0b41fc1fa7990f8a46677cb60d78a02f8cd09560
Viktor Kronvall <viktor.kronvall@gmail.com>
2024-06-19 01:24:01 +0900

    render method
    This commit replaces the side-effecting `__init__` constructor with a
    `render()` method on the `Element`. This allows delaying attaching
    an element until the method is called which in turn allows using
    elements as first-class values.

--- edifice/__init__.py
+++ edifice/__init__.py
@@ -129,7 +129,7 @@ To ensure control over the rerender process, it is recommended to :func:`Element
 whenever you have many children of the same class.
 """
 
-from .engine import QtWidgetElement, Element, component, Reference, child_place
+from .engine import QtWidgetElement, Element, component, Reference
 from .app import App
 from .utilities import alert, file_dialog, set_trace
 from .hooks import use_state, use_effect, use_async, use_ref, use_async_call, use_effect_final
