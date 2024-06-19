8e8bb5009a10adb4319a6429c64521734e6b324c
JoanCoCo <joacoco@inf.upv.es>
2024-06-19 16:46:29 +0900

    extras: Add numpy extras.

--- edifice/extra/__init__.py
+++ edifice/extra/__init__.py
@@ -1,4 +1,5 @@
 from .matplotlib_figure import MatplotlibFigure
+from .numpy_image import NumpyImage, NumpyArray, NumpyArray_to_QImage
 from .pyqtgraph_plot import PyQtPlot
 
-__all__ = ["MatplotlibFigure", "PyQtPlot"]
+__all__ = ["MatplotlibFigure", "PyQtPlot", "NumpyImage", "NumpyArray", "NumpyArray_to_QImage"]
