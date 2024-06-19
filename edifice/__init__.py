71531502d59b894962b6a5d46c53e1818666c3c8
James Brock <jamesbrock@gmail.com>
2024-06-19 15:50:19 +0900

    qt_component fixup

--- edifice/__init__.py
+++ edifice/__init__.py
@@ -129,7 +129,7 @@ To ensure control over the rerender process, it is recommended to :func:`Element
 whenever you have many children of the same class.
 """
 
-from .engine import QtWidgetElement, Element, component, Reference
+from .engine import QtWidgetElement, Element, component, Reference, qt_component
 from .app import App
 from .utilities import alert, file_dialog, set_trace
 from .hooks import use_state, use_effect, use_async, use_ref, use_async_call, use_effect_final
