4c5ff49f7baccd9a298e978659c92dbe0f2aa746
James Brock <jamesbrock@gmail.com>
2024-09-12 11:32:14 +0900

    example images

--- examples/images.py
+++ examples/images.py
@@ -1,5 +1,6 @@
 import os
 import typing as tp
+from pathlib import Path
 
 from edifice import App, HBoxView, Image, VBoxView, Window, component
 from edifice.qt import QT_VERSION
@@ -9,20 +10,36 @@ if QT_VERSION == "PyQt6" and not tp.TYPE_CHECKING:
 else:
     from PySide6.QtCore import Qt
 
-imgpath = os.path.join(os.path.dirname(__file__), "example_calculator.png")
-
-imgstyle = {"width": "200px", "height": "200px"}
+imgpath = str(Path(__file__).parent.parent / "docs/source/image/textinput_button.png")
 
 
 @component
 def MyComponent(self):
     with VBoxView():
         with HBoxView():
-            Image(src=imgpath, style=imgstyle)
-            Image(src=imgpath, aspect_ratio_mode=Qt.AspectRatioMode.IgnoreAspectRatio, style=imgstyle)
+            Image(src=imgpath)
+            Image(
+                src=imgpath,
+                style={"padding": "50px", "border": "10px solid black"},
+            )
         with HBoxView():
-            Image(src=imgpath, aspect_ratio_mode=Qt.AspectRatioMode.KeepAspectRatio, style=imgstyle)
-            Image(src=imgpath, aspect_ratio_mode=Qt.AspectRatioMode.KeepAspectRatioByExpanding, style=imgstyle)
+            Image(src=imgpath, style={"width": "200px", "height": "200px"})
+            Image(
+                src=imgpath,
+                aspect_ratio_mode=Qt.AspectRatioMode.IgnoreAspectRatio,
+                style={"width": "200px", "height": "200px"},
+            )
+        with HBoxView():
+            Image(
+                src=imgpath,
+                aspect_ratio_mode=Qt.AspectRatioMode.KeepAspectRatio,
+                style={"width": "200px", "height": "200px"},
+            )
+            Image(
+                src=imgpath,
+                aspect_ratio_mode=Qt.AspectRatioMode.KeepAspectRatioByExpanding,
+                style={"width": "200px", "height": "200px"},
+            )
 
 
 @component
