83aa3025c151520a4be978ea578e588673c44e04
James Brock <jamesbrock@gmail.com>
2025-01-15 23:06:35 +0900

    run_subprocess_with_callback docs

--- src/edifice/run_subprocess_with_callback.py
+++ src/edifice/run_subprocess_with_callback.py
@@ -1,4 +1,26 @@
-# run_subprocess_with_callback ©2025 pyedifice and James D. Brock
+# MIT License
+#
+# Edifice Copyright 2021 David Ding
+# Edifice run_subprocess_with_callback Copyright 2025 James D. Brock
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 
 from __future__ import annotations
 
