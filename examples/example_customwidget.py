2fc732116b3c8efb28b5d9d5934e39dd8730c61b
James Brock <jamesbrock@gmail.com>
2025-10-14 22:39:52 +0900

    example_customwidget.py

--- examples/example_customwidget.py
+++ examples/example_customwidget.py
@@ -15,21 +15,18 @@ class CustomCheckbox(ed.CustomWidget[QtWidgets.QCheckBox]):
     def __init__(self, checked: bool, text: str = "", on_change: tp.Callable[[bool], None] | None = None, **kwargs):
         super().__init__(**kwargs)
         self._register_props({"checked": checked, "text": text, "on_change": on_change})
-        self.on_change = on_change
 
     def create_widget(self) -> QtWidgets.QCheckBox:
         return QtWidgets.QCheckBox()
 
     def update(self, widget: QtWidgets.QCheckBox, diff_props: ed.PropsDiff) -> None:
         match diff_props.get("checked"):
-            case (_, checked):
-                assert isinstance(checked, bool)
+            case (_, bool(checked)):
                 widget.blockSignals(True)
                 widget.setChecked(checked)
                 widget.blockSignals(False)
         match diff_props.get("text"):
-            case (_, text):
-                assert isinstance(text, str)
+            case (_, str(text)):
                 widget.setText(text)
         match diff_props.get("on_change"):
             case (on_change_old, on_change_new):
@@ -43,13 +40,15 @@ class CustomCheckbox(ed.CustomWidget[QtWidgets.QCheckBox]):
 def Main(self):
     ed.use_palette_edifice()
     checked, checked_set = ed.use_state(False)
+    text, text_set = ed.use_state("Checked")
 
     with ed.Window(title="Custom Widget Example", _size_open=(300, 100)):
-        with ed.HBoxView(style={"padding": 10, "align": "center"}):
+        with ed.VBoxView(style={"padding": 10, "align": "center"}):
+            ed.TextInput(text, on_change=text_set, style={"font-size": 20})
             CustomCheckbox(
                 checked=checked,
                 on_change=checked_set,
-                text="Checked" if checked else "Not Checked",
+                text=text if checked else "Not " + text,
                 style={"font-size": 20, "font-style": "italic"},
             )
 
